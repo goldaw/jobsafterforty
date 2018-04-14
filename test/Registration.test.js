3edb864f7f1ad43bc746e426123d2b279e7f50cb
Anat Dagan <anatdaganster@gmail.com>
2018-04-14 09:07:32 +0300

    eslint

--- test/Registration.test.js
+++ test/Registration.test.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import Registration from '../src/Registration.js';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import Renderer from 'react-test-renderer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-  // Needed for onTouchTap
-  import injectTapEventPlugin from 'react-tap-event-plugin';
-  injectTapEventPlugin();
+// Needed for onTouchTap
+
+
+injectTapEventPlugin();
 it('renders shallow correctly', () => {
   const renderer = new ShallowRenderer();
-  renderer.render(<Registration/>);
+  renderer.render(<Registration />);
   const tree = renderer.getRenderOutput();
   expect(tree).toMatchSnapshot();
 });
 it('opens the form', () => {
   const renderer = new ShallowRenderer();
-  renderer.render(<Registration/>);
+  renderer.render(<Registration />);
   const tree0 = renderer.getRenderOutput();
   expect(tree0).toMatchSnapshot();
-  tree0.props.children.props.children[0].props.onTouchTap()
+  tree0.props.children.props.children[0].props.onTouchTap();
   const tree1 = renderer.getRenderOutput();
   expect(tree1).toMatchSnapshot();
 });
 it('renders correctly', () => {
-
   const tree = Renderer
-    .create(<MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}><Registration/></MuiThemeProvider>)
+    .create(<MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}><Registration /></MuiThemeProvider>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
