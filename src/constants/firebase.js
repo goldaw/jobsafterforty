cc65ba83f6fba6214ad7e39d2a0416d0e05f8117
golda302 <goldawillman@gmail.com>
2018-06-13 13:37:57 +0300

    Revert "config"
    This reverts commit 6085a2da93e4a35726126aaf0bde912fd53ddeec.

--- src/constants/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-    firebaseConfig: {
-      apiKey: 'AIzaSyCFHQkBg1J6YbL8R1JGaizpfqnG1ZTJj3w',
-      authDomain: 'jobsafterforty-staging.firebaseapp.com',
-      databaseURL: 'https://jobsafterforty-staging.firebaseio.com',
-      storageBucket: 'jobsafterforty-staging.appspot.com',
-    },
-  };
